c47ab0f73a79743628a1ef6af5a137513396eff2
elmkallu-maker <elmkallu@gmail.com>
2026-02-08 02:00:02 +0200

    Add HTTP handler to respond with a greeting

--- api/index.go
+++ api/index.go
@@ -1,1 +1,11 @@
+package handler
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello from Vercel! Path: %s", r.URL.Path)
+}
 
